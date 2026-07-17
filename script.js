acf2fcc726470cbb9b5ebbd9c3900628583d00c5
Salmankhan223 <ss2171531@gmail.com>
2026-07-17 19:51:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 
 const userScorepara = document.querySelector("#user-score")
-const compScorepara = document.querySelector("#comp-score")
+const compScorepara = document.querySelector("#comp-score") 
 const genCompChoice = () => {
     const options = ["rock", "paper", "scissor"];
     const randIdx = Math.floor(Math.random() * 3);
